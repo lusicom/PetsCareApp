24572ea234cb222a7facf40cdb201eb4efea569e
Lucy Holub <lusicomgolub@gmail.com>
2021-03-22 16:53:35 +0200

    adding basic models edits

--- app/src/main/java/com/example/petscareapp/Pet.java
+++ app/src/main/java/com/example/petscareapp/Pet.java
@@ -10,8 +10,8 @@ public class Pet {
     String photo;
     String awards;
     String diseases;
-    boolean vaccinated;
-    boolean aggressive;
+    boolean isVaccinated;
+    boolean isAggressive;
     String info;
 
     public Pet(String name, String gender, double age, PetType petType, double weight,
@@ -25,81 +25,81 @@ public class Pet {
         this.photo = photo;
         this.awards = awards;
         this.diseases = diseases;
-        this.vaccinated = vaccinated;
-        this.aggressive = aggressive;
+        this.isVaccinated = vaccinated;
+        this.isAggressive = aggressive;
         this.info = info;
     }
 
 
-    public void setPetName(String name){
+    public void setPetName(String name) {
         this.name = name;
     }
 
-    public void changePetName(String name){
+    public void changePetName(String name) {
         this.name = name;
     }
 
-    public void setPetGender(String gender){
+    public void setPetGender(String gender) {
         this.gender = gender;
     }
 
-    public void changePetGender(String gender){
+    public void changePetGender(String gender) {
         this.gender = gender;
     }
 
-    public void setPetAge(int age){
+    public void setPetAge(int age) {
         this.age = age;
     }
 
-    public void changePetAge(int age){
+    public void changePetAge(int age) {
         this.age = age;
     }
 
-    public void selectTypeOfPet(PetType petType){
+    public void selectTypeOfPet(PetType petType) {
         this.petType = petType;
     }
 
-    public void changeTypeOfPet(PetType petType){
+    public void changeTypeOfPet(PetType petType) {
         this.petType = petType;
     }
 
-    public void setPetWeight(double weight){
+    public void setPetWeight(double weight) {
         this.weight = weight;
     }
 
-    public void changePetWeight(double weight){
+    public void changePetWeight(double weight) {
         this.weight = weight;
     }
 
-    public void addPetAwards(String awards){
+    public void addPetAwards(String awards) {
         this.awards = awards;
     }
 
-    public void deletePetAward(String awards){
+    public void deletePetAward(String awards) {
         this.awards = awards;
     }
 
-    public void addDisease(String diseases){
+    public void addDisease(String diseases) {
         this.diseases = diseases;
     }
 
-    public void deleteDisease(String diseases){
+    public void deleteDisease(String diseases) {
         this.diseases = diseases;
     }
 
-    public void isVaccinated(boolean vaccinated){
-        this.vaccinated = vaccinated;
+    public void isVaccinated(boolean vaccinated) {
+        this.isVaccinated = vaccinated;
     }
 
-    public void isAggressive(boolean aggressive){
-        this.aggressive = aggressive;
+    public void isAggressive(boolean aggressive) {
+        this.isAggressive = aggressive;
     }
 
-    public void addInfoAboutPet(String info){
+    public void addInfoAboutPet(String info) {
         this.info = info;
     }
 
-    public void changeInfoAboutPet(String info){
+    public void changeInfoAboutPet(String info) {
         this.info = info;
     }
 
